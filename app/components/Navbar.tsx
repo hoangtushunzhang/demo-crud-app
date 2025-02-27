964b62299d83bf9ba11c75ecadf864fb30550868
Nguyen Xuan Quang <quang456a@gmail.com>
2025-02-27 15:22:55 +0700

    first commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between items-center border-b-2 px-20 py-3'>
+        <h1 className='text-3xl font-bold'>Demo</h1>
+       <Button>Sign In</Button>
+    </div>
   )
 }
 
